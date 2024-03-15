525f33859df3ad75d620f7326adc9d21911c28d8
RishabTR <rishab.official.8@gmail.com>
2024-03-15 23:00:18 +0530

    on mar 15

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,73 @@
-function Body(){
-    return(
-        <>
-            <p>Hello this is for main</p>
-        </>
-    )
+function Body() {
+  return (
+    <>
+      <section id="landing__area" class="container__center">
+        <div class="landing__content center__row">
+          <div class="landing__info align__left">
+            <h1>
+              Helping animals. <br />
+              Saving lives.
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+              aut ea fuga cum? Odit, sed culpa harum laborum excepturi
+              quibusdam!
+            </p>
+          </div>
+          <div class="image__pet"></div>
+        </div>
+        <section id="help" class="container__center">
+          <h1>Find out how to help the animals</h1>
+          <div class="help__cards--container center__row">
+            <div class="help__card container__center">
+              <span class="card__title">Adopting</span>
+              <h3>Give the animal you love a home</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+                quam ex neque.
+              </p>
+              <div class="card__link container__center">
+                <a href="#">Adopt Now</a>
+              </div>
+            </div>
+            <div class="help__card container__center">
+              <span class="card__title">Volunteering</span>
+              <h3>Be part of the team helping the animals</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+                quam ex neque.
+              </p>
+              <div class="card__link container__center">
+                <a href="#">Join Now</a>
+              </div>
+            </div>
+            <div class="help__card container__center">
+              <span class="card__title">Charity</span>
+              <h3>Help the Pets Care Foundation</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+                quam ex neque.
+              </p>
+              <div class="card__link container__center">
+                <a href="#">Help Now</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section id="gallery">
+            <img src="https://vapingdaily.com/wp-content/uploads/2019/03/Man-with-labrador-outdoors.jpg" class="img__gallery" id="img-1"/>
+            <img src="https://www.thesprucepets.com/thmb/SQspUyB0fpa6FaB9OeAOxyoBo8w=/4090x2721/filters:fill(auto,1)/cat-playing-with-toys-628829142-5911ee5c5f9b58647098d81c.jpg" class="img__gallery" id="img-2"/>
+            <img src="https://media.npr.org/assets/img/2020/03/27/dogs-coronavirus-7ddabfde75121f5020c307e134cb60dfa746fe1c-s800-c85.jpg" class="img__gallery" id="img-3"/>
+            <img src="https://images.squarespace-cdn.com/content/v1/5246fb63e4b0240948a40520/1440452356949-6A285K6C39FE7F8Y2C37/ke17ZwdGBToddI8pDm48kBtpJ0h6oTA_T7DonTC8zFdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIWkiAYz5ghgEgSGJuDQ4e1ZKXpRdhEMT7SgthRpD0vyIKMshLAGzx4R3EDFOm1kBS/image-asset.jpeg" class="img__gallery" id="img-4"/>
+            <img src="https://i0.wp.com/www.techdigest.tv/wp-content/uploads/2014/11/spotify-cat-people.jpg?ssl=1" class="img__gallery" id="img-5"/>
+            <img src="https://i1.wp.com/movingtips.wpengine.com/wp-content/uploads/2019/08/birds-transporting.jpg?fit=1024%2C684&ssl=1" class="img__gallery" id="img-6"/>
+            <img src="https://i.pinimg.com/originals/01/f3/11/01f31165f552ca9b5ac6857f6c59fa09.jpg" class="img__gallery" id="img-7"/>
+            <img src="https://www.bluecross.org.uk/sites/default/files/d8/assets/images/108048.jpg" class="img__gallery" id="img-8"/>
+            <img src="https://www.petsourcing.com/wp-content/uploads/2018/12/waht-is-the-most-popular-pets-in-Japan-696x602.jpg" class="img__gallery" id="img-9"/>
+        </section>
+    </>
+  );
 }
 
 export default Body;
